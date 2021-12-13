a448cabe59835dc8689ed455d11b756493d7d8a6
azhong <zczwzw@163.com>
2021-12-13 23:12:47 +0800

    update

---
+++ 04-KNN/02-kNN-in-scikit-learn/kNN_function/kNN.py
@@ -0,0 +1,28 @@
+import numpy as np
+from numpy import ndarray
+from math import sqrt
+from collections import Counter
+
+
+def kNN_classfy(k: int, X_train: ndarray, y_train: ndarray, x: ndarray) -> int:
+    '''
+    X_train: array([[3.39353321, 2.33127338],
+                    [3.11007348, 1.78153964],
+                    [1.34380883, 3.36836095],
+                    [3.58229404, 4.67917911],
+                    [2.28036244, 2.86699026],
+                    [7.42343694, 4.69652288],
+                    [5.745052  , 3.5339898 ],
+                    [9.17216862, 2.51110105],
+                    [7.79278348, 3.42408894],
+                    [7.93982082, 0.79163723]])
+
+    y_train: array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
+    x: array([8.09360732, 3.36573151])
+    '''
+    distances = [sqrt(np.sum((x_train - x)**2)) for x_train in X_train]
+    nearest = np.argsort(distances)
+
+    topK_y = [y_train[i] for i in nearest[:k]]
+    votes = Counter(topK_y)
+    return votes.most_common(1)[0][0]
